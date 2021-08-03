38bba84ec1ec2ae8b1409cb47a8a82eecc013d0e
Diego Oliveira <diego.oliveira@ficatranquilo.com.br>
2021-08-03 07:34:42 -0300

    adding logs

--- pkg/users/service.go
+++ pkg/users/service.go
@@ -7,6 +7,7 @@ import (
 	"github.com/d-leme/tradew-users/pkg/core"
 	"github.com/golang-jwt/jwt"
 	"github.com/google/uuid"
+	"github.com/sirupsen/logrus"
 )
 
 type service struct {
@@ -25,19 +26,29 @@ func NewService(settings *core.Settings, repository Repository) Service {
 // Create ...
 func (s *service) Create(ctx context.Context, correlationID string, req *CreateUserRequest) (*CreateUserResponse, error) {
 
+	fields := logrus.Fields{
+		"email":          req.Email,
+		"correlation_id": correlationID,
+	}
+
 	password, err := NewPassword(req.Password)
 	if err != nil {
+		logrus.WithError(err).WithFields(fields).Error("password validation failed")
 		return nil, err
 	}
 
 	user, err := NewUser(uuid.NewString(), req.Name, req.Email, password)
 	if err != nil {
+		logrus.WithError(err).WithFields(fields).Error("user validation failed")
 		return nil, err
 	}
 
 	if err := s.repository.Insert(ctx, user); err != nil {
+		logrus.WithError(err).WithFields(fields).Error("error while inserting user")
 		return nil, err
 	}
+
+	logrus.WithFields(fields).Info("user created successfully")
 
 	return &CreateUserResponse{ID: user.ID}, nil
 }
@@ -45,13 +56,23 @@ func (s *service) Create(ctx context.Context, correlationID string, req *CreateU
 // Login ...
 func (s *service) Login(ctx context.Context, correlationID string, req *LoginRequest) (*LoginResponse, error) {
 
+	fields := logrus.Fields{
+		"email":          req.Email,
+		"correlation_id": correlationID,
+	}
+
 	user, err := s.repository.GetByEmail(ctx, req.Email)
 	if err != nil {
+		logrus.WithError(err).WithFields(fields).Error("error while getting user")
+		return nil, err
+	}
+
+	if user == nil {
 		return nil, core.ErrInvalidCredentials
 	}
 
 	if err = user.Password.Equal(req.Password); err != nil {
-		return nil, core.ErrInvalidCredentials
+		return nil, err
 	}
 
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
@@ -61,8 +82,11 @@ func (s *service) Login(ctx context.Context, correlationID string, req *LoginReq
 
 	tokenString, err := token.SignedString([]byte(s.settings.JWT.Secret))
 	if err != nil {
+		logrus.WithError(err).WithFields(fields).Error("error while generating token")
 		return nil, err
 	}
+
+	logrus.WithFields(fields).Error("logged in successfully")
 
 	return &LoginResponse{Token: tokenString}, nil
 }
